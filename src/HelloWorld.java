42ab12e66b652b2bdcee6fe220b7656a9baac46a
prakashid2 <prakashid2@gmail.com>
2022-09-14 19:37:04 +0000

    modified HelloWorld.java to print the date in another formmat

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.text.*;
+
 /**
  * @author Lenovo
  *
@@ -5,5 +7,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World !!");
+		java.util.Date dt = new java.util.Date();
+
+		SimpleDateFormat dateformat2 = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
+		System.out.println("Date in another formmat "+dateformat2.format(dt));
 	}
 }
