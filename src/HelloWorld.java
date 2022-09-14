a4564bd67dfdd6624250602fdd650ed2610268a0
prakashid2 <prakashid2@gmail.com>
2022-09-14 19:09:27 +0000

    added HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,20 +1,9 @@
-import java.text.*;
-
 /**
- * Hello World program to print current date and time
  * @author Lenovo
  *
  */
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World !!");
-		java.util.Date dt = new java.util.Date();
-		SimpleDateFormat dateformat = new SimpleDateFormat("dd/MM/yyyy  HH:mm:ss z");
-		
-		System.out.println("Current Time = "+dateformat.format(dt));
-		SimpleDateFormat dateformat2 = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
-		System.out.println("Date in another formmat "+dateformat2.format(dt));
-		
-		System.out.print("For SimpleDateFormat pls refer - https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html");
 	}
 }
